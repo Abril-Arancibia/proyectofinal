8c4b952b786c129a48068ec0dda0e52ab082bfae
abril_arancibia <abrilarancibia@outlook.com>
2021-03-27 22:33:30 -0300

    Eventos

--- public/index.js
+++ public/index.js
@@ -69,11 +69,12 @@ const productsContainer = document.getElementById("products-container");
 
 productos.forEach(function(producto) {
     const productoDiv = document.createElement('div');
-    productoDiv.className = "product"
+    productoDiv.classList= "product addToCart"
 
     const figureContainer = document.createElement('figure');
     const imagenContainer = document.createElement('img');
     imagenContainer.src = producto.imagen;
+    imagenContainer.className = "imgProduct";
 
     const productoNombre = document.createElement('div');
     productoNombre.className = "name"
@@ -106,4 +107,52 @@ productos.forEach(function(producto) {
     figureContainer.appendChild(imagenContainer);
 
 });
+    
+const addToShoppingCartButtons = document.querySelectorAll(".addToCart");
+addToShoppingCartButtons.forEach(addToCartButton => {
+    addToCartButton.addEventListener("click", addToCartClicked);
+});
 
+const shoppingCartProductsContainer= document.querySelector(".products-container");
+
+function addToCartClicked(event){
+    const button = event.target;
+    const product = button.closest(".product");
+
+    const name = product.querySelector(".name").textContent;
+    const price = product.querySelector(".price").textContent;
+    const img = product.querySelector(".imgProduct").src;
+
+ addProductToShoppingCart(name, price, img);
+}
+
+function addProductToShoppingCart(name, price, img) {
+    const shoppingContainer = document.getElementById('shoppingCartProductsContainer');
+    const shoppingCartRow = document.createElement("div");
+    const shoppingCartContent = `<div class="row shoppingCartProduct">
+    <div class="col-6">
+        <div class="shopping-cart-product d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <img src=${img} class="shopping-cart-image">
+            <h6 class="shopping-cart-product-name shoppingCartProductName text-truncate ml-3 mb-0">${name}
+            </h6>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <p class="product-price mb-0 shoppingCartProductPrice">${price}</p>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+            <input class="shopping-cart-quantity-input shoppingCartProductQuantity" type="number" value="1">
+            <button class="btn btn-danger buttonDelete" type="button">X</button>
+        </div>
+    </div>
+</div>`;
+shoppingCartRow.innerHTML = shoppingCartContent;
+shoppingContainer.appendChild(shoppingCartRow);
+
+
+
+
+}
